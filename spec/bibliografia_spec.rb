c1fa3dbbaa521a489bc9862d3f9265630a9c2f5b
alu0100816761 <alu0100816761@ull.edu.es>
2015-11-17 22:02:32 +0000

    Variable del periodico actualizada

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -238,12 +238,12 @@ end
 
 describe Periodico do
     before :all do
-        @r1 = Periodico.new
+        @p1 = Periodico.new
     end
     
     describe "# comprobar la instancia del objeto" do
         it "Es una instancia de Revista" do
-           (@r1.instance_of?Periodico).should eq(true)
+           (@p1.instance_of?Periodico).should eq(true)
         end
     end
 end
