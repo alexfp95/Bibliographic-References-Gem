2d3bb59930c9159fb7d2de6db5767fd548b265dd
alu0100816761 <alu0100816761@ull.edu.es>
2015-12-08 19:22:18 +0000

    Añadido el codigo para el formateo de la cita de una Revista.

--- lib/bibliografia/cita.rb
+++ lib/bibliografia/cita.rb
@@ -50,9 +50,15 @@ class Cita
     def to_s
         if @ref.instance_of?Libro
             cadena = "#{get_autores}(#{@ref.anno}).  "
+            cadena = "#{cadena}"+"Titulo del libro: #{@ref.get_titulo} (#{@ref.get_edicion}).  "
+            cadena = "#{cadena}"+"Lugar de publicacion: #{@ref.get_editorial}.  "
+        end
+        if @ref.instance_of?Revista
+            cadena = "#{get_autores}(#{@ref.anno}).  "
             cadena = "#{cadena}"+"Titulo del libro: #{get_titulo} (#{@ref.get_edicion}).  "
             cadena = "#{cadena}"+"Lugar de publicacion: #{@ref.get_editorial}.  "
         end
+        cadena
     end
     
 end
