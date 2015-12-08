88612c7543cc0b277932dafcc122f6efbced031d
alu0100816761 <alu0100816761@ull.edu.es>
2015-12-08 16:09:50 +0000

    Añadido al codigo linea 'Autor, '. Prueba superada

--- lib/bibliografia/cita.rb
+++ lib/bibliografia/cita.rb
@@ -9,6 +9,7 @@ class Cita
     end
 
     def get_autores
+        cadena = "Autor, "
         cadena = "#{cadena}"+"#{@ref.autores[1][0]}. "
         cadena = "#{cadena}"+"#{@ref.autores[0][0]}. "
     end
